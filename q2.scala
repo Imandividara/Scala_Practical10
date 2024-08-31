ff37d21bc663136fe4e8613e66cbeeefc4851166
Imandi Muthugala <imandividaramuthugala@gmail.com>
2024-08-31 18:10:38 +0530

    done_scala

--- q2.scala
+++ q2.scala
@@ -1,35 +1,38 @@
-class Rational(n: Int, d: Int) {
-  require(d != 0, "Denominator cannot be zero")
+object RationalNumbers {
 
-  // Greatest Common Divisor
-  private def gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
+  case class Rational(numerator: Int, denominator: Int) {
+    require(denominator != 0, "Denominator cannot be zero")
 
-  // Simplified numerator and denominator
-  private val g = gcd(n.abs, d.abs)
-  val numerator: Int = n / g
-  val denominator: Int = d / g
+    // Reduce the fraction to its simplest form
+    private def gcd(a: Int, b: Int): Int = {
+      if (b == 0) a else gcd(b, a % b)
+    }
 
-  // Negate method
-  def neg: Rational = new Rational(-numerator, denominator)
+    private val divisor = gcd(numerator, denominator)
+    val reducedNumerator = numerator / divisor
+    val reducedDenominator = denominator / divisor
 
-  // Subtract method
-  def sub(that: Rational): Rational = {
-    val newNumerator = this.numerator * that.denominator - that.numerator * this.denominator
-    val newDenominator = this.denominator * that.denominator
-    new Rational(newNumerator, newDenominator)
+    // Method to subtract two Rational numbers
+    def subtract(other: Rational): Rational = {
+      val newNumerator = (this.reducedNumerator * other.reducedDenominator) - (other.reducedNumerator * this.reducedDenominator)
+      val newDenominator = this.reducedDenominator * other.reducedDenominator
+      Rational(newNumerator, newDenominator)
+    }
+
+    override def toString: String = s"$reducedNumerator/$reducedDenominator"
   }
 
-  // Overriding toString for better display
-  override def toString: String = s"$numerator/$denominator"
+  def main(args: Array[String]): Unit = {
+    val x = Rational(3, 4)
+    val y = Rational(5, 8)
+    val z = Rational(2, 7)
+
+    val resultYMinusZ = y.subtract(z)
+    val result = x.subtract(resultYMinusZ)
+
+    println(s"x - (y - z) = $result")
+  }
 }
 
-object RationalTest extends App {
-  val x = new Rational(3, 4)   // 3/4
-  val y = new Rational(5, 8)   // 5/8
-  val z = new Rational(2, 7)   // 2/7
 
-  // Calculate x - y - z
-  val result = x.sub(y).sub(z)
 
-  println(s"Result of $x - $y - $z = $result")
-}
